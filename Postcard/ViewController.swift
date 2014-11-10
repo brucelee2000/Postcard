584aa1d0d0781e74819c630688256e8fa5c988e3
brucelee2000 <brucelee2000@tp.edu.tw>
2014-11-10 15:49:58 -0800

    Finished 1st and 2nd chanllege.

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
 
     override func viewDidLoad() {
@@ -44,6 +45,12 @@ class ViewController: UIViewController {
         mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
         mailButton.backgroundColor = UIColor.greenColor()
         
+        // Update label properties
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
     }
 
 }
